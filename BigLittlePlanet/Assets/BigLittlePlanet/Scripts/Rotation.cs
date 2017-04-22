cd05b5d5182a3c756d8e87b71ab4f796e425c71a
Alessandro Miralles <meenners@gmail.com>
2017-04-21 21:40:46 -0500

    adding bullshit

---
+++ BigLittlePlanet/Assets/BigLittlePlanet/Scripts/Rotation.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+namespace BigLittlePlanet
+{
+    public class Rotation : MonoBehaviour
+    {
+
+        #region Public Attributes
+        public float rotationSpeed = 20.0F;
+        #endregion
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
+        }
+    }
+}
---
+++ BigLittlePlanet/Assets/BigLittlePlanet/Scripts/SoundFx.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BigLittlePlanet
+{
+    public class SoundFx : MonoBehaviour
+    {
+
+        #region Public Attributes
+        public AudioClip audioClip;
+        #endregion
+
+        #region Private Variable
+        private AudioSource _audioSource { get { return GetComponent<AudioSource>();  } }
+        #endregion
+
+
+        // Use this for initialization
+        void Start()
+        {
+            gameObject.AddComponent<AudioSource>();
+            _audioSource.clip = audioClip;
+            _audioSource.playOnAwake = true;
+        }
+
+        public void Play()
+        {
+            _audioSource.PlayOneShot(audioClip);
+        }
+    }
+}
