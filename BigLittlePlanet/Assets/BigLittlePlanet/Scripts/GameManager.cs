2e7701a6bea9b8de4441d6b31b3a3a41aec6c512
Alex Carlson <alex@acwd.me>
2017-04-22 00:28:54 -0500

    test

---
+++ BigLittlePlanet/Assets/BigLittlePlanet/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public int score = 0;
+
+    public Text scoreText;
+
+    public void ScorePoints(int points = 1)
+    {
+        score = score + points;
+        scoreText.text = score.ToString();
+    }
+}
