fab90893bee10e5222ef315508933dc2b263d30a
Alessandro Miralles <meenners@gmail.com>
2017-04-23 08:15:01 -0500

    adding more

---
+++ Assets/Scripts/RandomizeProp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeProp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //wtf not workin???? TODO:
+        transform.position = new Vector3(transform.position.x, Random.Range(-2, 2), transform.position.z);
+    }
+}
